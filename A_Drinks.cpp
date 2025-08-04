11dc98f6b4240cbcc0e9c26443177605980c6447
mahmoud-fathi443 <mahmoudfathi443@gmail.com>
2025-08-04 16:15:39 +0300

    A

---
+++ A_Drinks.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int n;
+    double sum = 0, cocktail_perc;
+    
+    
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        int drink_percentage;
+        cin >> drink_percentage;
+        sum += drink_percentage;
+    }
+
+    
+    cocktail_perc = sum / n;
+    
+    cout << cocktail_perc << endl;
+    
+
+    return 0;
+}
