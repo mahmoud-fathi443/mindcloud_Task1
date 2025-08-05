a27247e2cfab32ae5d2453a581e41c31dd7e50b7
mahmoud-fathi443 <mahmoudfathi443@gmail.com>
2025-08-05 15:38:09 +0300

    D

---
+++ D_Diagonal_Difference.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int  diagonalDifference(vector<vector<int>> arr){
+    int primary_diagonal = 0;
+    int secondary_diagonal = 0;
+    int abs_diff;
+
+    for(int i = 0 ; i < arr.size(); i++){
+        for(int j = 0; j < arr.size(); j++){
+            if(i == j) primary_diagonal+= arr[i][j];
+            if((i+j) == (arr.size() - 1)) secondary_diagonal+= arr[i][j];
+        }
+        cout << "\n";
+    }
+
+    abs_diff = abs(primary_diagonal - secondary_diagonal);
+    return abs_diff;
+}
+
+int main(){
+    int n;
+    cin >> n;
+
+    vector<vector<int>> arr(n, vector<int>(n));
+    for(int i = 0 ; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+
+    int abs_diff = diagonalDifference(arr);
+    cout << abs_diff << endl;
+    
+
+    return 0;
+}
